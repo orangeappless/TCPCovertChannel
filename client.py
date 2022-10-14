ee26b5c059708002a5eefee19681ebb9d6bcd571
Wilfred Ta <wilf.ta0072@gmail.com>
2022-10-14 00:39:59 -0700

    added encryption with Fernet

--- client.py
+++ client.py
@@ -2,12 +2,23 @@
 
 
 from scapy.all import *
+from cryptography.fernet import Fernet
 
 
 def create_packet(character):
     pkt = IP(dst="192.168.1.81", id=character)/TCP(flags="S", dport=8505)
     
     return pkt
+
+
+def encrypt_data(data):
+    with open("keyfile.key", "rb") as keyfile:
+        key = keyfile.read()
+
+    fernet = Fernet(key)
+    encrypted_data = fernet.encrypt(data)
+
+    return encrypted_data
 
 
 def main():
@@ -18,12 +29,17 @@ def main():
             print("Exiting program...")
             break
         
-        data += ";"
-        ascii_data = [ord(c) for c in data]
+        data = encrypt_data(data.encode("utf-8"))
+        data += b";"
+        decoded_data = data.decode("utf-8")
+
+        ascii_data = [ord(c) for c in decoded_data]
+        # print(decoded_data)
+        # print(type(decoded_data))
 
         for char in ascii_data:
             covert_pkt = create_packet(char)
-            send(covert_pkt)
+            send(covert_pkt, verbose=False)
 
     sys.exit()
 
--- server.py
+++ server.py
@@ -2,15 +2,33 @@
 
 
 from scapy.all import *
+from cryptography.fernet import Fernet
 
 
 def read_pkt(packet):
     packet_id = packet[IP].id
    
-    if chr(packet_id) == ";":
-        print("\n")
-    else:
-        print(chr(packet_id), end="", flush=True)
+    with open("encrypted_covert.txt", "a") as file:
+        if chr(packet_id) == ";":
+            file.write("\n")
+            decrypt_data()
+        else:
+            file.write((chr(packet_id)))
+
+
+def decrypt_data():
+    with open("keyfile.key", "rb") as keyfile:
+        key = keyfile.read()
+    
+    fernet = Fernet(key)
+
+    with open("encrypted_covert.txt", "rb") as file:
+        lines = file.read().splitlines()
+        last_line = lines[-1]
+
+        last_line_decrypted = fernet.decrypt(last_line)
+
+        print(last_line_decrypted.decode("utf-8"))
 
 
 def main():
